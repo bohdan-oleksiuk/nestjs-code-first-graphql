48e29e1ba3b65b56e881a15b97b1791111035452
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-06-08 12:45:35 +0300

    init typeorm

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCoffeeInput } from "./dto/create-coffee.input";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Coffee } from "./entities/coffee.entity";
+import { Repository } from "typeorm";
+import { UserInputError } from "apollo-server-express";
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee) private readonly coffeesRepo: Repository<Coffee>,
+  ) {}
+
   async findAll() {
-    return [];
+    return this.coffeesRepo.find();
   }
 
   async findOne(id: number) {
-    return null;
+    const coffee = await this.coffeesRepo.findOne({ where: { id } });
+    if (!coffee) {
+      throw new UserInputError(`Coffee #${id} does not exist`);
+    }
+    return coffee;
   }
 
   async create(createCoffeeInput: CreateCoffeeInput) {
-    return null;
+    const coffee = this.coffeesRepo.create(createCoffeeInput);
+    return this.coffeesRepo.save(coffee);
   }
 }
