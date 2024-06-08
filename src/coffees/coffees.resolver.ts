58ce61a74d2464c5120f45c1f4866972ea776b9d
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-06-08 12:21:28 +0300

    coffee service

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,21 +1,24 @@
 import { Resolver, Query, Args, ID, Mutation } from "@nestjs/graphql";
 import { Coffee } from "./entities/coffee.entity";
 import { CreateCoffeeInput } from "./dto/create-coffee.input";
+import { CoffeesService } from "./coffees.service";
 
 @Resolver()
 export class CoffeesResolver {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Query(() => [Coffee], { name: 'coffees' })
   async findAll() {
-    return [];
+    return this.coffeesService.findAll();
   }
 
   @Query(() => Coffee, { name: 'coffee', nullable: true })
   async findOne(@Args('id', { type: () => ID }) id: number) {
-    return null;
+    return this.coffeesService.findOne(id);
   }
 
   @Mutation(() => Coffee, { name: 'createCoffee', nullable: true })
   async create(@Args('createCoffeeInput') createCoffeeInput: CreateCoffeeInput) {
-    return null;
+    return this.coffeesService.create(createCoffeeInput);
   }
 }
