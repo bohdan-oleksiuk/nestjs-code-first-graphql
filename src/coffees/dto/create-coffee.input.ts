acdfd5606e356fa510cee3b7c9bd07b96989f1ac
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-06-08 13:25:48 +0300

    add pipes

--- src/coffees/dto/create-coffee.input.ts
+++ src/coffees/dto/create-coffee.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { MinLength } from "class-validator";
 
 @InputType({ description: 'Create coffee input object type.' })
 export class CreateCoffeeInput {
+  @MinLength(3)
   @Field(() => String, { description: 'Coffee name' })
   name: string;
   brand: string;
